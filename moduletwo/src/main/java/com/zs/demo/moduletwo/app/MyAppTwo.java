1e16fdec5aa03c7e1ba5157b541ea41906194955
QQzs <552146383@qq.com>
2019-01-03 15:51:00 +0800

    build

---
+++ moduletwo/src/main/java/com/zs/demo/moduletwo/app/MyAppTwo.java
@@ -0,0 +1,19 @@
+package com.zs.demo.moduletwo.app;
+
+import com.zs.demo.baselib.BaseApplication;
+
+/**
+ * Created by zs
+ * Date：2019年 01月 02日
+ * Time：13:27
+ * —————————————————————————————————————
+ * About:
+ * —————————————————————————————————————
+ */
+public class MyAppTwo extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
